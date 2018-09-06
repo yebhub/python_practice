5c8b04090e7d218c793aeaf6d5ebac96715ed754
BackStreetSoy <jordanedwardobey@gmail.com>
2018-09-06 12:19:44 -0400

    added a child car class

--- car.py
+++ car.py
@@ -28,7 +28,12 @@ class Car():
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.update_odometer(23)
-my_new_car.read_odometer() 
-my_new_car.increment_odometer(100)
-my_new_car.read_odometer()
+
+class ElectricCar(Car): 
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
