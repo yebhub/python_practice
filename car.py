95f0761a91860ff8939eac66c0d6519a5f763565
BackStreetSoy <jordanedwardobey@gmail.com>
2018-09-06 13:37:35 -0400

    created a battery class

--- car.py
+++ car.py
@@ -33,7 +33,23 @@ class ElectricCar(Car):
 
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("this car has a " + str(self.battery_size) + "-kWh battery.")
+
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
